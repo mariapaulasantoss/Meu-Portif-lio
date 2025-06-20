3fb8b1a3ad08a677d25d28eb873c7511e8c7afb8
mariapaulasantoss <mariahpaula668@gmail.com>
2025-06-20 09:32:02 -0300

    adicao de responsividade

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const cabecalho_subtitulo = document.querySelector(".cabecalho_subtitulo");
 cabecalho_subtitulo.textContent = ''; 
 
@@ -13,19 +12,3 @@ function efeito_digitação(texto, contador){
 }
 
 efeito_digitação("Seja muito bem vindo(a)!", 0);
-=======
-const cabecalho_subtitulo = document.querySelector(".cabecalho_subtitulo");
-cabecalho_subtitulo.textContent = ''; 
-
-function efeito_digitação(texto, contador){
-    if(contador < texto.length) {
-        setTimeout(() => {
-            cabecalho_subtitulo.textContent += texto.charAt(contador);
-            contador++;
-            efeito_digitação(texto, contador); 
-        }, 120);
-    }
-}
-
-efeito_digitação("Seja muito bem vindo(a)!", 0);
->>>>>>> 258db2667cb8859d758546a631f43faf37b2d7a0
